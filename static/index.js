6c1c0b84e98cf203eb083396b4141192927d506b
Jimmy <chen.jimmym@gmail.com>
2017-12-10 10:22:13 -0800

    added navbar

--- static/index.js
+++ static/index.js
@@ -1,5 +1,21 @@
+
 $(document).ready(function () {
-    
+
+    $('body').scrollspy({ target: ".navbar", offset: 50 });
+    $("#navbar_buttons a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+                window.location.hash = hash;
+            });
+        }
+
+    });
+
     var words = ["is a discipline.     ", "requires patients.     ", "is always solvable.     "];
     var scroll = document.getElementById('title_scroll');
     var wordCounter = 0;
@@ -10,9 +26,9 @@ $(document).ready(function () {
         if (characterCounter == words[wordCounter].length) {
             characterCounter = 0;
             wordCounter++;
-            setTimeout(function(){}, 1000);
+            setTimeout(function () { }, 1000);
             scroll.innerHTML = ' ';
-            if (wordCounter == words.length){
+            if (wordCounter == words.length) {
                 wordCounter = 0;
             }
         }
